6799d2474f7ca38b907b81091a6765fb7c67146a
Sahadad <brisapinanki@gmail.com>
2018-10-15 15:28:26 -0500

    segundo pushote

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const cors         = require('cors')
 
 
 mongoose
-  .connect('mongodb://localhost/back', {useNewUrlParser: true})
+  .connect('process.env.DB', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
