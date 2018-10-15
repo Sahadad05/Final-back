37b2603b12519f155c5309cd165d106034e6a9ac
Sahadad <brisapinanki@gmail.com>
2018-10-15 15:34:32 -0500

    tercer piushote

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const cors         = require('cors')
 
 
 mongoose
-  .connect('process.env.DB', {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
